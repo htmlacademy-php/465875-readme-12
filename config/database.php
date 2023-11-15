2b099fb5f7d769959ac80128b75a7beeabffdc93
kaganov5 <kalaganov5@gmail.com>
2023-11-15 10:58:51 +0300

    Чтение и показ записей из MySQL

---
+++ config/database.php
@@ -0,0 +1,9 @@
+<?php
+
+$database = [
+    'host'     => '45.130.41.184',
+    'database' => 'kalaganov5_readm',
+    'user'     => 'kalaganov5_readm',
+    'password' => 'tYV0Xt7&',
+    'port'     => '3306'
+];
